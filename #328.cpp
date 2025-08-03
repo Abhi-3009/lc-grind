ff89abd7700ce9b7958271d1cce78f37102104b9
Abhijeet <raiabhijeet3009@gmail.com>
2025-08-03 23:40:59 +0530

    Med:Odd Even Linked List

---
+++ #328.cpp
@@ -0,0 +1,19 @@
+ListNode *oddEvenList(ListNode *head)
+{
+    if (head == NULL || head->next == NULL)
+        return head;
+
+    ListNode *odd = head;
+    ListNode *even = head->next;
+    ListNode *temp = even;
+    while (even && even->next)
+    {
+        odd->next = odd->next->next;
+        odd = odd->next;
+
+        even->next = even->next->next;
+        even = even->next;
+    }
+    odd->next = temp;
+    return head;
+}

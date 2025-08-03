628c4a20cf42dc7c7654f9678f73ab0584a0b67d
Abhijeet <raiabhijeet3009@gmail.com>
2025-08-04 01:04:36 +0530

    Med:Partition List

---
+++ #086.cpp
@@ -0,0 +1,27 @@
+ListNode *partition(ListNode *head, int x)
+{
+    ListNode *low = new ListNode(0);
+    ListNode *high = new ListNode(0);
+    ListNode *low_end = low;
+    ListNode *high_end = high;
+
+    while (head)
+    {
+        if (head->val < x)
+        {
+            low_end->next = head;
+            low_end = low_end->next;
+        }
+        else
+        {
+            high_end->next = head;
+            high_end = high_end->next;
+        }
+        head = head->next;
+    }
+    high_end->next = NULL;
+    low_end->next = high->next;
+
+    ListNode *new_head = low->next;
+    return new_head;
+}

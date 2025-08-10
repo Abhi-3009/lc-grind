aa226907f922f5e1db64329b3c0b2388af44891d
Abhijeet <raiabhijeet3009@gmail.com>
2025-08-11 00:18:59 +0530

    Easy:Missing Number

---
+++ #0268.cpp
@@ -0,0 +1,11 @@
+int missingNumber(vector<int> &nums)
+{
+    int sum = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        sum += nums[i];
+    }
+    int N = (nums.size() * (nums.size() + 1)) / 2;
+
+    return N - sum;
+}

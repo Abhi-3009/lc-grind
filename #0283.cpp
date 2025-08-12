47f2f7636b4e8011eba971c48f3f97f2c0620001
Abhijeet <raiabhijeet3009@gmail.com>
2025-08-12 14:38:24 +0530

    Easy:Move Zeroes

---
+++ #0283.cpp
@@ -0,0 +1,11 @@
+void moveZeroes(vector<int> &nums)
+{
+    int index = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] != 0)
+            nums[index++] = nums[i];
+    }
+    while (index < nums.size())
+        nums[index++] = 0;
+}

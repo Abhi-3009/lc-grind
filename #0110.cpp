d9bf4c15674b00fe70d66235c2d277c8d4721971
Abhijeet <raiabhijeet3009@gmail.com>
2025-09-19 18:14:57 +0530

    Easy:Balanced Binary Tree

--- #0110.cpp
+++ #0110.cpp
@@ -19,3 +19,32 @@ bool isBalanced(TreeNode *root)
         return false;
     return isBalanced(root->left) && isBalanced(root->right);
 }
+
+// Time Complexity: O(nlogn) in worst case
+// Space Complexity: O(h) where h is the height of the tree
+
+// Optimized Approach
+int depth(TreeNode *root)
+{
+    if (!root)
+        return 0;
+    int lst = depth(root->left);
+    if (lst == -1)
+        return -1;
+
+    int rst = depth(root->right);
+    if (rst == -1)
+        return -1;
+
+    if (abs(lst - rst) > 1)
+        return -1;
+    return 1 + max(lst, rst);
+}
+
+bool isBalanced(TreeNode *root)
+{
+    return depth(root) != -1;
+}
+
+// Time Complexity: O(n) in worst case
+// Space Complexity: O(h) where h is the height of the tree

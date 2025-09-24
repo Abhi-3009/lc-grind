bbc7e6d3ba2d0a05b062bbea1c31d3436ab291f2
Abhijeet <raiabhijeet3009@gmail.com>
2025-09-24 15:46:01 +0530

    Easy:Symmetric Tree

---
+++ #0101.cpp
@@ -0,0 +1,16 @@
+bool helper(TreeNode *p, TreeNode *q)
+{
+    if (!p && !q)
+        return true;
+    if (!p || !q)
+        return false;
+    if (p->val != q->val)
+        return false;
+
+    return helper(p->left, q->right) && helper(p->right, q->left);
+}
+
+bool isSymmetric(TreeNode *root)
+{
+    return helper(root->left, root->right);
+}

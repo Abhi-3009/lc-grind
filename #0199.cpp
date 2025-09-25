8dfededdd2b039b98d833775a6af129bb986a56f
Abhijeet <raiabhijeet3009@gmail.com>
2025-09-25 10:43:30 +0530

    Med:Binary Tree Right Side View

---
+++ #0199.cpp
@@ -0,0 +1,16 @@
+vector<int> res;
+void dfs(TreeNode *root, int depth)
+{
+    if (!root)
+        return;
+    if (res.size() == depth)
+        res.push_back(root->val);
+    dfs(root->right, depth + 1);
+    dfs(root->left, depth + 1);
+}
+
+vector<int> rightSideView(TreeNode *root)
+{
+    dfs(root, 0);
+    return res;
+}

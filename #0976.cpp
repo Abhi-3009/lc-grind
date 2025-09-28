d8b7b625941fc6fba7bde0c1898a530982a2dafe
Abhijeet <raiabhijeet3009@gmail.com>
2025-09-29 01:34:29 +0530

    Easy:Largest Perimeter Triangle

---
+++ #0976.cpp
@@ -0,0 +1,10 @@
+int largestPerimeter(vector<int> &nums)
+{
+    sort(nums.begin(), nums.end(), greater<int>());
+    for (int i = 2; i < nums.size(); i++)
+    {
+        if (nums[i] + nums[i - 1] > nums[i - 2])
+            return nums[i] + nums[i - 1] + nums[i - 2];
+    }
+    return 0;
+}
